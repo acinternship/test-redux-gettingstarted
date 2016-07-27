14759732702b1cbe27bb8b380a40e39259607dd9
Pedro Felipe Froes <pedrofelipefroes@gmail.com>
2016-07-27 14:22:19 -0300

    Redux increment app sucessfully implemented

--- js/main.js
+++ js/main.js
@@ -1,5 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
 
-ReactDOM.render( < App / > , document.getElementById('app'))
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    
+    case 'DECREMENT':
+      return state - 1
+    
+    default:
+      return state
+  }
+}
+
+const Counter = ({value, onIncrement, onDecrement}) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
+)
+
+const store = createStore(counter);
+
+const App = () => {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => store.dispatch({type: 'INCREMENT'})}
+      onDecrement={() => store.dispatch({type: 'DECREMENT'})}
+    />,
+    document.getElementById('app')
+  )
+}
+
+store.subscribe(App)
+
+App()
