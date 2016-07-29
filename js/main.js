a73fbd365b86db960904c2dca1a06137983cf40a
Pedro Felipe Froes <pedrofelipefroes@gmail.com>
2016-07-29 16:25:51 -0300

    Refactored React classes and implemented ... operator across objects

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 
-/*** REDUX TODO APP ***/
+/*** REDUX STORE ***/
 
-// REDUX STORE
-const counter = (state = [], action) => {
+const todoapp = (state = [], action) => {
+// Handles every action of Todo App, such as adding,
+// editing, removing and toggling a task
   switch (action.type) {
     case 'ADD_TODO':
       return [
@@ -17,25 +18,110 @@ const counter = (state = [], action) => {
         }
       ]
     
+    case 'EDIT_TODO':
+      return state
+      
+    case 'REMOVE_TODO':
+      return state.splice(action.id, 1)
+    
+    case 'TOGGLE_TODO':
+      return state.map(todo => {
+        if (todo.id !== action.id) return todo
+        
+        return {
+          ...todo,
+          completed: !todo.completed
+        }
+      })
+      
     default:
       return state
   }
 }
 
-const store = createStore(counter)
+const store = createStore(todoapp)
 
-// REACT COMPONENTS
+/*** REACT COMPONENTS ***/
 
-const TodoItem = (props) => (
-    <tr>
-      <td>{props.data.id}</td>
-      <td>{props.data.text}</td>
-    </tr>
-)
+class NewTodo extends React.Component {
+// Create a new task
+  constructor() {
+    super()
+    
+    this.state = {text: 'Type in a new task'}
+    
+    this.setText = this.setText.bind(this)
+    this.addTodo = this.addTodo.bind(this)
+  }
+  
+  setText(event) {
+    this.setState({text: event.target.value})
+  }
+  
+  addTodo() {
+    store.dispatch({
+      type: 'ADD_TODO',
+      id: store.getState().length,
+      text: this.state.text
+    })
+  }
+  
+  render() {
+    return (
+      <newtodocontainer className="input-group">
+        <input className="input-group-field" type="text" value={this.state.text} onChange={this.setText}/>
+        <div className="input-group-button">
+          <input type="submit" className="button" value="Add item" onClick={this.addTodo}></input>
+        </div>
+      </newtodocontainer>
+    )
+  }
+}
 
-var TodoList = React.createClass({
-  render: function() {
-    let rows = store.getState().map(
+class TodoItem extends React.Component {
+// Edit, remove and toggle a single task to 'done'
+  
+  constructor(props) {
+    super()
+    
+    this.removeTodo = this.removeTodo.bind(this)
+    this.toggleTodo = this.toggleTodo.bind(this)
+  }
+  
+  editTodo() {} // Still to be implemented
+  
+  removeTodo() {
+    store.dispatch({
+      type: 'REMOVE_TODO',
+      id: this.props.data.id
+    })
+  }
+  
+  toggleTodo(event) {
+    store.dispatch({
+      type: 'TOGGLE_TODO',
+      id: this.props.data.id
+    })
+  }
+  
+  render() {
+    return (
+      <tr>
+        <td>
+          <input type="radio" onClick={this.toggleTodo}></input>
+        </td>
+        <td>{this.props.data.id},{this.props.data.text}</td>
+        <td onClick={this.removeTodo}>x</td>
+      </tr>
+    )
+  }
+}
+
+class TodoList extends React.Component {
+// Renders every task item already added
+  
+  render() {
+    let todoitems = store.getState().map(
       item => <TodoItem key={item.id} data={item}/>
     )
     
@@ -43,49 +129,20 @@ var TodoList = React.createClass({
       <table className="hover">
         <thead>
           <tr>
-            <th width="100"></th>
+            <th width="50"></th>
             <th width="500"></th>
+            <th width="50"></th>
           </tr>
         </thead>
         <tbody>
-          {rows}
+          {todoitems}
         </tbody>
       </table>
     )
   }
-})
+}
 
-var NewTodo = React.createClass({
-  getInitialState: function() {
-    return {text: 'Type in a new task'}
-  },
-  
-  updateText: function(event) {
-    this.setState({text: event.target.value})
-  },
-  
-  addTodo: function() {
-    store.dispatch({
-      type: 'ADD_TODO',
-      id: store.getState().length,
-      text: this.state.text
-    })
-    this.state.text = 'Type in a new task'
-  },
-  
-  render: function() {
-    return (
-      <newtodocontainer className="input-group">
-        <input className="input-group-field" type="text" value={this.state.text} onChange={this.updateText}/>
-        <div className="input-group-button">
-          <input type="submit" className="button" value="Add item" onClick={this.addTodo}></input>
-        </div>
-      </newtodocontainer>
-    )
-  }
-})
-
-// OUTPUT
+/*** OUTPUT ***/
 
 const App = () => {
   console.log(store.getState())
